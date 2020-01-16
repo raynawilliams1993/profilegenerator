0256b981577cd3622d4b976b99b093f7b894810f
rayna williams <Raynawilliams123@gmail.com>
2020-01-16 01:25:02 -0700

    barebones index.js starter code from previous activities in class

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
     return inquirer.prompt([ 
-
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "location",
+            message: "Where are you from?"
+        },
+        {
+            type: "input",
+            name: "hobby",
+            message: "What is your favorite hobby?"
+        },
+        {
+            type: "input",
+            name: "food",
+            message: "What is your favorite food?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub Username"
+        },
+        {
+            type: "input",
+            name: "linkedin",
+            message: "Enter your LinkedIn URL."
+        }
     ]);
 }
