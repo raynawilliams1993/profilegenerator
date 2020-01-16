f03293874a4981c4331a9022c746e313a63c292c
rayna williams <Raynawilliams123@gmail.com>
2020-01-16 15:14:59 -0700

    color scheme completed

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-
+const generateHtml = require("./generatehtml");
 const writeFileAsync = util.promisify(fs.writeFile);
+const axios = require("axios");
 
 function promptUser() {
     return inquirer.prompt([ 
         {
             type: "input",
-            name: "name",
-            message: "What is your name?"
+            name: "userName",
+            message: "What is your github user name?"
         },
         {
-            type: "input",
-            name: "location",
-            message: "Where are you from?"
+            type: "list",
+            name: "color",
+            message: "What is your favorite color?",
+            choices:["green","blue","pink","red"]
         },
-        {
-            type: "input",
-            name: "hobby",
-            message: "What is your favorite hobby?"
-        },
-        {
-            type: "input",
-            name: "food",
-            message: "What is your favorite food?"
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "Enter your GitHub Username"
-        },
-        {
-            type: "input",
-            name: "linkedin",
-            message: "Enter your LinkedIn URL."
-        }
-    ]);
-}
+    ]).then(function(ans){
+        let data = {color: ans.color}
+    })
