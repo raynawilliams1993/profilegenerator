e4cc1859ef7126678a8cf55415832e61be1805a5
rayna williams <Raynawilliams123@gmail.com>
2020-01-16 23:02:44 -0700

    added the generate pdf to html function

--- index.js
+++ index.js
@@ -20,27 +20,42 @@ function promptUser() {
         },
     ]).then(function (ans) {
         let data = { color: ans.color }
-        gitHub(ans.userName,data);
+        gitHub(ans.userName, data);
     })
 }
-function gitHub (username,data) {
+function gitHub(username, data) {
     const url = `https://api.github.com/users/${username}`;
-    axios.get(url).then(function(res) {
-       console.log(res.data);
-       console.log(res.data.bio);
-       console.log(res.data.avatar_url);
-       data["bio"] = res.data.bio;
-       data["profile_pic"] = res.data.avatar_url;
-       data ["location"] = res.data.location;
-       data["user_blog"] = res.data.blog;
-       data ["followers"] = res.data.followers_url;
-       data["following"] = res.data.following_url;
-       data["starred_repositories"] - res.data.starred_url;
-       data["number_of_public_repos"] = res.data.public_repos;
-       
+    axios.get(url).then(function (res) {
+        console.log(res.data);
+        console.log(res.data.bio);
+        console.log(res.data.avatar_url);
+        data["bio"] = res.data.bio;
+        data["profile_pic"] = res.data.avatar_url;
+        data["location"] = res.data.location;
+        data["user_blog"] = res.data.blog;
+        data["followers"] = res.data.followers_url;
+        data["following"] = res.data.following_url;
+        data["starred_repositories"] - res.data.starred_url;
+        data["number_of_public_repos"] = res.data.public_repos;
 
-       generateHtml(data)
-        });
-  
-}   
+        generateHtml(data)
+    });
+    var fs = require('fs'),
+        convertFactory = require('electron-html-to');
+
+    var conversion = convertFactory({
+        converterPath: convertFactory.converters.PDF
+    });
+
+    conversion({ html: '<h1>Hello World</h1>' }, function (err, result) {
+        if (err) {
+            return console.error(err);
+        }
+
+        console.log(result.numberOfPages);
+        console.log(result.logs);
+        result.stream.pipe(fs.createWriteStream('/path/to/anywhere.pdf'));
+        conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+    });
+}
 promptUser();
